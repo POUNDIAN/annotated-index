65d64dced2a72f15879a2800cdd5e54bc54d2823
Louis de Beaumont <debeaumont.louis@gmail.com>
2023-08-18 12:19:17 +0100

    Extract cantos per entry

--- scripts/idk.py
+++ scripts/idk.py
@@ -3,8 +3,18 @@ import re
 from utils.getter import annotated_index
 
 ai = annotated_index()
+out = {}
 
 for entry in ai:
     page_numbers = ai[entry]["Page Numbers"]
-    if re.match("[a-zA-Z]", page_numbers):
-        print(f"{entry}: {page_numbers}")
+    search = re.findall("\d+\/", page_numbers)
+    if search:
+        # Remove '/'
+        search = [s[:-1] for s in search]
+
+        # Save to entry
+        ai[entry]["cantos"] = search
+
+        # This loses information if we have multiple instances in one canto.
+
+print(ai)
